8f80604bba93536dd90daf49e74a1b01d37d8285
gagarin55 <gagarin55@protonmail.com>
2017-03-10 10:51:11 +0300

    block,height commands

--- index.js
+++ index.js
@@ -9,39 +9,64 @@ const rpc = Waves.client(host);
 
 
 function statusHandler(self) {
-    const statusFuture  = rpc.getNodeStatus();
+    const statusFuture = rpc.getNodeStatus();
     const versionFuture = rpc.getNodeVersion();
 
-    return Promise.all([versionFuture, statusFuture]).
-      then(function(results){
-         self.log(chalk.bold('                Version: ')+results[0]);
-         self.log(chalk.bold('        Block generator: ')+results[1].blockGeneratorStatus);
-         self.log(chalk.bold('           History sync: ')+results[1].historySynchronizationStatus);
-      });
+    return Promise.all([versionFuture, statusFuture]).then(function (results) {
+        self.log(chalk.bold('                Version: ') + results[0]);
+        self.log(chalk.bold('        Block generator: ') + results[1].blockGeneratorStatus);
+        self.log(chalk.bold('           History sync: ') + results[1].historySynchronizationStatus);
+    });
 };
 
 function poolHandler(self) {
     const future = rpc.getUnconfirmedTransactions();
-    return future.then(function(result){
-      result.forEach(function(tx){
-        self.log(tx.id+' '+tx.amount+' '+tx.sender+'->'+tx.recipient);
-      });
+    return future.then(function (result) {
+        result.forEach(function (tx) {
+            self.log(tx.id + ' ' + tx.amount + ' ' + tx.sender + '->' + tx.recipient);
+        });
     });
 };
 
-vorpal
-  .command('status', 'Get node version and status')
-  .action(function(args, callback) {
-    return statusHandler(this);
-  });
+function blockHandler(self, args) {
+    const height = args.height;
+    const future = rpc.getBlockAt(height)
+        .then(function (block) {
+            self.log(block);
+        });
+    return future;
+};
 
 vorpal
-  .command('pool', 'Get unconfirmed transactions')
-  .action(function(args){
-     return poolHandler(this);
-   });
+    .command('status', 'Get node version and status')
+    .action(function (args, callback) {
+        return statusHandler(this);
+    });
 
 vorpal
-  .delimiter('waves#')
-  .show();
+    .command('pool', 'Get unconfirmed transactions')
+    .action(function (args) {
+        return poolHandler(this);
+    });
+
+vorpal
+    .command('block <height>', 'Get block')
+    .action(function (args, cb) {
+        blockHandler(this, args);
+        cb();
+    });
+
+vorpal
+    .command('height', 'Get current height')
+    .action(function (args) {
+        const self = this;
+        return rpc.getHeight()
+            .then(function (height) {
+                self.log(height);
+            });
+
+    });
+vorpal
+    .delimiter('waves#')
+    .show();
 
